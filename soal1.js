cc6028f2a7462acfc45c520af290dd5743974799
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2025-04-06 22:56:51 +0700

    Update soal1.js

--- soal1.js
+++ soal1.js
@@ -1,11 +1,7 @@
-const prompt = require("prompt-sync")();
-
 function Calculate(reviews) {
-    let minRating = Math.min(...reviews); //Nilai Minimum
-    let maxRating = Math.max(...reviews); //Nilai maksimum
-    //...reviews = menampilkan semua elemen array reviews
-
-    //Mengembalikan nilai rata-rata elemen pada reviews
+    let minRating = Math.min(...reviews);
+    let maxRating = Math.max(...reviews);  
+    
     let total = 0;
     for (let i = 0; i < reviews.length; i++) {
         total += reviews[i];
@@ -15,13 +11,17 @@ function Calculate(reviews) {
     return [minRating, maxRating, avgRating];
 }
 
-let input = prompt("Masukkan rating aplikasi (pisahkan dengan koma): "); //Contoh Input: 4.5, 3, 5, 2.8,...
-let inputArr = input.split(',').map(item => parseFloat(item)).filter(num => !isNaN(num));
+let inputArr1 = [4.5, 2.0, 1.5, 3.0, 2.5, 4.0, 5.0, 3.5, 2.0, 1.0];
+let inputArr2 = [5,4,2.5,5,3.6,1.1,3.6,4,4.2,1.5];
 
-if (inputArr.length > 0) {
-    let result = Calculate(inputArr)
+const Hasil1 = Calculate(inputArr1);
+const Hasil2 = Calculate(inputArr2);
+
+if (Hasil1.length > 0 && Hasil2.length > 0) {
     console.log("[Min, Max, Avg]");
-    console.log(result);
+    console.log(Hasil1);
+    console.log("[Min, Max, Avg]");
+    console.log(Hasil2);
 }else{
     console.log("Tidak ada DATA RATING");
 }
