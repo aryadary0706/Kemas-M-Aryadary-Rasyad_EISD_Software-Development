1e1fd7aab649745bee784d94ce9ddb6aafe85582
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2025-04-06 21:55:33 +0700

    Update soal2.js

--- soal2.js
+++ soal2.js
@@ -2,9 +2,9 @@ const prompt = require("prompt-sync")();
 
 function Palindrome(text){
     let cleanedText = text.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-    console.log(cleanedText)
+    // console.log(cleanedText)
     let reversedText = cleanedText.split('').reverse().join('');
-    console.log(reversedText)
+    // console.log(reversedText)
     
     if (cleanedText === reversedText) {
         return "eureeka!";
