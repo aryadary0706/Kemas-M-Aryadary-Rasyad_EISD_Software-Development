c0701186405ccf79f23cbc5391b71ff51c68285d
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2025-04-06 22:57:11 +0700

    Update soal10.js

--- soal10.js
+++ soal10.js
@@ -3,7 +3,6 @@ const prompt = require("prompt-sync")();
 function hitungKembalian(totalBayar, totalBelanja) {
     let kembalian = totalBayar - totalBelanja;
 
-    // Cek jika uang yang dibayarkan kurang
     if (kembalian < 0) {
         console.log("Uang yang dibayarkan tidak cukup.");
         return;
@@ -27,11 +26,8 @@ function hitungKembalian(totalBayar, totalBelanja) {
     for (let key in hasil) {
         console.log(`Rp${key} x ${hasil[key]}`);
     }
-    // console.log(hasil)
 }
 
-// Input seperti cin di C++
-let bayarInput = parseInt(prompt("Masukkan jumlah uang yang dibayarkan: "));
-let belanjaInput = parseInt(prompt("Masukkan total harga belanja: "));
-
-hitungKembalian(bayarInput, belanjaInput);
+hitungKembalian(10000, 7500);
+hitungKembalian(5000, 1100);
+hitungKembalian(178000, 90500);
