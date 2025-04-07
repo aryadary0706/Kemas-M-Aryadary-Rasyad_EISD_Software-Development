f6697d6650058da52050d0e6b1653e151fc8d116
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2025-04-07 08:39:47 +0700

    Update soal9.js

--- soal9.js
+++ soal9.js
@@ -1,30 +1,37 @@
-const prompt = require("prompt-sync")();
-
+// Memanggil fungsi analisaAnakNakals dengan array nama anak
 function analisaAnakNakal(namaAnak) {
+    // Menghitung frekuensi sebutan nama
     const frekuensi = {};
     namaAnak.forEach(nama => {
         frekuensi[nama] = (frekuensi[nama] || 0) + 1;
     });
 
+    // Mencari frekuensi tertinggi dalam objek frekuensi
     const nilaiTertinggi = Math.max(...Object.values(frekuensi));
 
+    // Mencari nama anak yang memiliki frekuensi sebutan paling tinggi
     const Anak = Object.entries(frekuensi)
         .filter(([_, count]) => count === nilaiTertinggi)
         .map(([nama]) => nama);
 
+    // Menentukan output
     if (Anak.length === namaAnak.length) {
         console.log("Semuanya anak baik");
     } else if (Anak.length === 1) {
-        console.log(`${Anak[0]} Nackal`);
+        console.log(`${Anak[0]} Nakal`);
     } else {
-        console.log(`${Anak.join(' dan ')} Nackal`);
+        console.log(`${Anak.join(' dan ')} Nakal`);
     }
 }
 
 const namaAnakArray1 = ["Bagas", "Dimas", "Bagas", "Bagas",  "Indra", "Gilang", "Gilang", "Hana",  "Fajar", "Fajar"];
 const namaAnakArray2 = ["Bagas", "Dimas", "Fajar", "Bagas",  "Indra", "Gilang", "Gilang", "Bagas",  "Fajar", "Fajar"];
-const namaAnakArray3 = ["Aisyah" , "Bagas", "Dewi", "Dimas",  "Eka", "Fajar", "Gilang" , "Hana", "Indra",  "Jihan"]
+const namaAnakArray3 = ["Aisyah" , "Bagas", "Dewi", "Dimas",  "Eka", "Fajar", "Gilang" , "Hana", "Indra",  "Jihan"];
 
+// Memanggil fungsi analisaAnakNakal dengan array nama anak
+console.log("Input 1");
 analisaAnakNakal(namaAnakArray1);
+console.log("Input 2");
 analisaAnakNakal(namaAnakArray2);
+console.log("Input 3");
 analisaAnakNakal(namaAnakArray3);
