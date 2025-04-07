8069fd96a0525efbe65846493dbe6004eb130d80
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2025-04-07 08:38:35 +0700

    Update soal4.js

--- soal4.js
+++ soal4.js
@@ -16,4 +16,4 @@ function cekDuplikat(arr) {
 let inputArr = [20, 1, 3, 2, 4, 6, 8, 5, 7, 9, 11, 13, 15, 10,  12, 14, 16, 18, 20, 17, 19];
 
 let hasil = cekDuplikat(inputArr);
-console.log(hasil);
+console.log("Dari array ", inputArr, "\n" ,hasil ? "Ditemukan adanya duplikat" : "Ditemukan tidak ada duplikat");
